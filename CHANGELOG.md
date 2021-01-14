4523b979c2ae4cfa45fa8099ea89c42c15037275
Mircea M <mircea.moise@gmail.com>
2021-01-14 10:23:39 +0000

    [Update gocd] Updated so it works with Go agent version 20.2.0+ using the new docker agent image

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,25 @@
+### 0.0.16 (2021-Jan-14)
+
+### 0.0.15 (2021-Jan-13)
+
+### 0.0.14 (2021-Jan-13)
+
+### 0.0.13 (2021-Jan-13)
+
+### 0.0.12 (2021-Jan-13)
+
+### 0.0.11 (2021-Jan-13)
+
+### 0.0.10 (2021-Jan-13)
+
+### 0.0.9 (2021-Jan-13)
+
+### 0.0.8 (2021-Jan-13)
+
+### 0.0.7 (2021-Jan-13)
+
+### 0.0.6 (2021-Jan-13)
+
 ### 0.0.3 (2020-Oct-07)
 
 Refactor ssm key to use the agreed convention (/repo/${GOCD_ENVIRONMENT}/user-input/autoregister-key)
--- image/run-on-start/setup-secrets.sh
+++ image/run-on-start/setup-secrets.sh
@@ -1,36 +1,72 @@
 #!/usr/bin/with-contenv bash
 
-set -e
+set -eo pipefail
 
-function read_aws_secret {
-  local secret_id=$1
-  aws ssm get-parameter --with-decryption --region $AWS_REGION --name $secret_id | jq -r ".Parameter.Value"
-  if [ $? != 0 ]; then
-    echo "Failed reading $secret_id from AWS SSM: ${secret_id}" >&2
+GOCD_HOME=/home/go
+AGENT_WORK_DIR="/go"
+
+function read_secret_or_die {
+  local vault_key=$1
+
+  if [ -z "${SECRET_STORE}" ]; then
+    echo "Secret store is not selected. Can't obtain secret: ${vault_key}" >&2
     exit 1
+  elif [ "${SECRET_STORE}" == "vault" ]; then
+    echo "Trying to read from vault: ${vault_key}." >&2
+    if [ -z "$VAULT_TOKEN" ]; then
+      echo "Failed reading from vault: VAULT_TOKEN is not set." >&2
+      exit 1
+    fi
+    if [ -z "$VAULT_ADDR" ]; then
+      echo "Failed reading from vault: VAULT_ADDR is not set." >&2
+      exit 1
+    fi
+    vault kv get --field=value secret/${VAULT_SECRET_STORE_PATH}/${vault_key}
+    if [ $? != 0 ]; then
+      echo "Failed reading from vault: ${vault_key}" >&2
+      exit 1
+    else
+      echo "Successfully read from vault: ${vault_key}" >&2
+    fi
+  elif [ "${SECRET_STORE}" == "aws" ]; then
+    echo "Trying to read from aws: ${vault_key}." >&2
+    if [ -z "$AWS_REGION" ]; then
+      echo "Failed reading from aws: AWS_REGION is not set." >&2
+      exit 1
+    fi
+    aws ssm get-parameter --with-decryption --region $AWS_REGION --name /${AWS_SECRET_STORE_PATH}/${vault_key} | jq -r ".Parameter.Value"
+    if [ $? != 0 ]; then
+      echo "Failed reading from aws: /${AWS_SECRET_STORE_PATH}/${vault_key}" >&2
+      exit 1
+    else
+      echo "Successfully read from aws: /${AWS_SECRET_STORE_PATH}/${vault_key}" >&2
+    fi
   else
-    echo "Successfully read $secret_id from AWS SSM: ${secret_id}" >&2
+    echo "Invalid or unsupported secret store: ${SECRET_STORE}" >&2
+    exit 1
   fi
+
+
 }
 
 if [[ "${!GOCD_SKIP_SECRETS[@]}" ]]; then
   echo "GOCD_SKIP_SECRETS is set - will not setup go's access to most of services"
   exit 0;
 fi
-
-
-if [ -z "$GOCD_ENVIRONMENT" ]; then
-  echo "GOCD_ENVIRONMENT is not set. It is needed for go agent to get autoregister key"
-  exit 3
-fi
-
-if [ -z "$AGENT_KEY" ]; then
-  echo "AGENT_KEY is not set. It is needed for go agent to autoregister"
+if [ -z "$AGENT_AUTO_REGISTER_KEY" ]; then
+  echo "AGENT_AUTO_REGISTER_KEY is not set but is needed for agent to autoregister. Falling back to secret store." >&2
   # the variables exported here are not visible in services' run files
-  AGENT_KEY=$(read_aws_secret "/repo/${GOCD_ENVIRONMENT}/user-input/autoregister-key")
+  # todo: rename the prod key
+  AGENT_AUTO_REGISTER_KEY=$(read_secret_or_die "autoregistration_key")
+fi
+if [ -z "$GOCD_SSH_KEY" ]; then
+  echo "GOCD_SSH_KEY is not set. It is needed for go agent to use git over ssh. Falling back to secret store." >&2
+  GOCD_SSH_KEY=$(read_secret_or_die "go_id_rsa")
 fi
 
-echo "export AGENT_KEY=${AGENT_KEY}" >> /var/go/gocd_agent_key
+echo "export AGENT_AUTO_REGISTER_KEY=${AGENT_AUTO_REGISTER_KEY}" >${GOCD_HOME}/gocd_AGENT_AUTO_REGISTER_KEY
 
-mkdir -p /var/go/
-chown go:go -R /var/go/.ssh
+# quotes are required to keep multiline file
+mkdir -p ${GOCD_HOME}/.ssh/
+
+chown go:go -R ${GOCD_HOME}/.ssh
